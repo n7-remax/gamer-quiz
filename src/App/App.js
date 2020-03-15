3ae6fb3982cd344493997dc9005b0f490fc5e38c
N7_Remax <ditritusa@gmail.com>
2020-03-15 20:27:18 +0200

    create state in app

--- src/App/App.js
+++ src/App/App.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './App.css';
-import Question from './Question/Question';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          React Quiz.
+class App extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      counter: 0,
+      questionId: 1,
+      question: '',
+      answerOptions: [],
+      answer: '',
+      answersCount: {},
+      result: ''
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            React Quiz
         </p>
-      </header>
-      <Question content="Test"/>
-    </div>
-  );
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
