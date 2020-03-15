46dd35baaf29d4d21aed149e79bbc70b68e1319e
N7_Remax <ditritusa@gmail.com>
2020-03-15 20:20:17 +0200

    created Quiz component

---
+++ src/App/Quiz/Quiz.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import QuestionCount from '../QuestionCount/QuestionCount';
+import Question from '../Question/Question';
+import AnswerOption from '../AnswerOption/AnswerOption';
+
+function renderAnswerOptions(key) {
+    return (
+      <AnswerOption
+        key={key.content}
+        answerContent={key.content}
+        answerType={key.type}
+        answer={props.answer}
+        questionId={props.questionId}
+        onAnswerSelected={props.onAnswerSelected}
+      />
+    );
+  }
+
+function Quiz(props) {
+    return (
+        <div className="quiz">
+          <QuestionCount
+            counter={props.questionId}
+            total={props.questionTotal}
+          />
+          <Question content={props.question} />
+          <ul className="answerOptions">
+            {props.answerOptions.map(renderAnswerOptions)}
+          </ul>
+        </div>
+    );
+  }
+    
+  export default Quiz;
