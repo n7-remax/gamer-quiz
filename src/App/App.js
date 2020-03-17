fc6cb68f8fd4ef5f80af057506314913105ff7cb
N7_Remax <ditritusa@gmail.com>
2020-03-17 16:40:39 +0200

    add shuffleArray method on mount

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './App.css';
 
+import quizQuestions from '../api/quizQuestions';
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -16,6 +18,35 @@ class App extends Component {
       result: ''
     };
   }
+
+  componentDidMount() {
+    const shuffledAnswerOptions = quizQuestions.map((question) => this.shuffleArray(question.answers));  
+  
+    this.setState({
+      question: quizQuestions[0].question,
+      answerOptions: shuffledAnswerOptions[0]
+    });
+  }
+
+  shuffleArray(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  };
+
   render() {
     return (
       <div className="App">
