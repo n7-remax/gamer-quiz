ab9dd27cc5a9ce2db25b1a359e2bcdbc54f6a732
N7_Remax <ditritusa@gmail.com>
2020-03-17 16:49:43 +0200

    add setNextQuestion method

--- src/App/App.js
+++ src/App/App.js
@@ -68,6 +68,18 @@ class App extends Component {
       }
   }
 
+  setNextQuestion() {
+    const counter = this.state.counter + 1;
+    const questionId = this.state.questionId + 1;
+    this.setState({
+      counter: counter,
+      questionId: questionId,
+      question: quizQuestions[counter].question,
+      answerOptions: quizQuestions[counter].answers,
+      answer: ''
+    });
+  }
+
   render() {
     return (
       <div className="App">
