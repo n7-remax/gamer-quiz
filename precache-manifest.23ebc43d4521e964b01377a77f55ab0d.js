a4e2163b5f7e70b04a135ab4cd4f249f770dc8a9
N7_Remax <ditritusa@gmail.com>
2020-03-20 17:11:51 +0200

    Updates

---
+++ precache-manifest.23ebc43d4521e964b01377a77f55ab0d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5d5e83aefb99a15c5f7fec14a8ed9e8",
+    "url": "/gamer-quiz/index.html"
+  },
+  {
+    "revision": "050d8aa0295ba58cc902",
+    "url": "/gamer-quiz/static/css/main.3b8d9eee.chunk.css"
+  },
+  {
+    "revision": "eeac90349d5935cbd1ee",
+    "url": "/gamer-quiz/static/js/2.43c1f300.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/gamer-quiz/static/js/2.43c1f300.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "050d8aa0295ba58cc902",
+    "url": "/gamer-quiz/static/js/main.ed5938e3.chunk.js"
+  },
+  {
+    "revision": "ded0f118ef97c046d783",
+    "url": "/gamer-quiz/static/js/runtime-main.2bb86d7b.js"
+  },
+  {
+    "revision": "a34a3680e3111e856bf1805340a79f9f",
+    "url": "/gamer-quiz/static/media/icon-check.a34a3680.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gamer-quiz/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/gamer-quiz/precache-manifest.23ebc43d4521e964b01377a77f55ab0d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gamer-quiz/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
